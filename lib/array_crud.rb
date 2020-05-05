e62690d114ffd51445b3d996ab96598b140db1af
Percy Major <chipercy@hotmail.com>
2020-05-05 01:55:07 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,24 +1,32 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+   ["1", "2", "3", "4"]
 end
 
 def add_element_to_end_of_array(array, element)
   
+  add_element_to_end_of_array.push("element")
+
 end
 
 def add_element_to_start_of_array(array, element)
+  
+  add_element_to_start_of_array.unshift("element")
   
 end
 
 def remove_element_from_end_of_array(array)
   
+  remove_element_from_end_of_array.pop("element")
+  
 end
 
 def remove_element_from_start_of_array(array)
+  
+  remove_element_from_start_of_array.shift("element")
   
 end
 
